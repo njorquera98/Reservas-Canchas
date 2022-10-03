4ebe084557f6a1875e3779804c8caf0c0f9f0028
njorquera98 <njorquera98.dev@gmail.com>
2022-10-03 18:59:49 -0300

    Routes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.use('/', require('./routes/router'))
 
 app.listen(3000, (req, res) => {
     console.log('Listening on port http://localhost:3000')
