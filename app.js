710137d6195e188f015508c64d1b07d9930786a5
Nicolas Jorquera Araya <72469933+njorquera98@users.noreply.github.com>
2022-10-23 13:38:36 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use('/', require('./routes/router'))
-
-app.listen(3000, (req, res) => {
-    console.log('Listening on port http://localhost:3000')
-})
-
